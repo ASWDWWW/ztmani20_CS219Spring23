7ac98c9e3ad062c471a0882b26428291f0070a1e
Zakiy Manigo <zakiymanigo@gmail.com>
2023-01-26 10:16:28 -0500

    commiting java file

--- src/HW1/Time.java
+++ src/HW1/Time.java
@@ -1,22 +1,31 @@
 package HW1;
 
 public class Time {
+    //void is the return type and main is the method name
     public static void main(String[] args) {
+        //header before the question answers
         System.out.println("HW1");
         //Q2
+        //static variables with values for the time
         int hour = 15;
         int minute = 23;
         int second = 20;
+        //computation of how many seconds is 2:23:20PM
         int from_midnight = (hour * 60 * 60) + (minute * 60) + second;
         //Q3
+        //print out the amount of seconds from midnight
         System.out.println("It has been " + from_midnight + " seconds from midnight");
+        //computation for seconds in a day
         int dayseconds = (24 * 60 * 60);
+        //computation for seconds left in the day
         int secondsleft = dayseconds - from_midnight;  // test
         //Q4
+        //print the amount of seconds left in a day
         System.out.println("There are " + secondsleft + " seconds left in the day");
-
+        //computation for percent of the day has passed
         int daypercent = from_midnight*100 / dayseconds;
         //Q5
+        //print the percent of the day has passed
         System.out.println("At 3:23:20, " + daypercent + "% of the day has passed");
     }
 
