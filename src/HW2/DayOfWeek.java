aa0f9af07f9b323e9b5691e1be41f7bdeea3d4e9
zakiymanigo <zakiymanigo@gmail.com>
2023-02-01 11:37:55 -0500

    changes and commenting for Homework 2 Day of the week program

--- src/HW2/DayOfWeek.java
+++ src/HW2/DayOfWeek.java
@@ -1,7 +1,10 @@
 package HW2;
 
+import java.util.Scanner;
+
 public class DayOfWeek {
     public static Integer dayofweek(int month, int day, int years) {
+        // computation for day of the week from moth day and year entered
         int y0 =  years - (14 - month)/12;
 
         int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
@@ -9,11 +12,25 @@ public class DayOfWeek {
         int m0 = month + 12* ((14 - month) / 12) - 2;
 
         int d0 = (day + x + 31*m0 / 12) % 7;
+
+        //return the day of the week corresponding to 1-7
         return d0;
 
     }
 
     public static void main(String[] args) {
-        System.out.print(dayofweek(2, 10, 2020));
+        //Read computer input for temp and wind vel from user
+        Scanner kbd =new Scanner(System.in);
+        //prompt before and then read the month
+        System.out.print("Enter month 1-12: ");
+        int month = kbd.nextInt();
+        //prompt before and then read the month
+        System.out.print("Enter day 1-31: ");
+        int day = kbd.nextInt();
+        //prompt before and then read the year
+        System.out.print("Enter full year xxxx: ");
+        int year = kbd.nextInt();
+        //print of the return of dayofweek function
+        System.out.print(dayofweek(month, day, year));
     }
 }
