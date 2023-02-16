6d4e35954740ab9a698d8386bd517e2905b8ab2b
zakiymanigo <zakiymanigo@gmail.com>
2023-02-15 20:55:53 -0500

    HW4 work for CS219 finished

--- src/HW4/HW4.java
+++ src/HW4/HW4.java
@@ -5,97 +5,102 @@ import java.util.Scanner;
 public class HW4 {
     public static void main(String[] args) {
         Scanner kbd = new Scanner(System.in);
-        double c = validateInvestmentInitial(kbd);
-        double r = validateInvestmentRate(kbd);
-        double t = validateInvestmentTime(kbd);
-        double n = validateInvestmentYears(kbd);
-
+        getValidInvestment(kbd);
+        getValidInterestRate(kbd);
+        getValidNumberOfYears(kbd);
+        getValidCompund(kbd);
     }
 
-    public static double validateInvestmentInitial(Scanner kbd) {
-        double i = 1;
+
+    public static void getValidInvestment(Scanner kbd) {
+        int i = 1;
         while (i == 1) {
             System.out.print("Enter the initial investment value: ");
-            double c = kbd.nextDouble();
             if (kbd.hasNextDouble()) {
+                double c = kbd.nextDouble();
                 if (c < 0) {
-                    System.out.printf("Error: Initial investment should be greater than zero. You entered: %f\n", c);
+                    System.out.printf("Error: Initial investment should be greater than zero. You entered: %.1f\n", c);
                 }
                 else {
-                    System.out.printf("Initial Investment: %f\n", c);
+                    System.out.printf("Initial Investment: %.1f\n", c);
+                    System.out.println();
                     i = 0;
                 }
             }
             else {
-                System.out.printf("Error: Initial investment should be a valid number. You entered: %f\n", c);
+                String d = kbd.next();
+                System.out.println("Error: Initial investment should be a valid number. You entered: " + d);
             }
         }
-        return i;
     }
 
-    public static double validateInvestmentRate(Scanner kbd) {
-        double i = 2;
+    public static void getValidInterestRate(Scanner kbd) {
+        int i = 2;
         while (i == 2) {
             System.out.print("Enter the investment rate: ");
-            double c = kbd.nextDouble();
+
             if (kbd.hasNextDouble()) {
-                if (c < 0) {
-                    System.out.printf("Error: Investment rate should be greater than zero. You entered: %f\n", c);
+                double c = kbd.nextDouble();
+                if (c < 0 || c > 1) {
+                    System.out.printf("Error: Investment rate should be between 0-1. You entered: %.1f\n", c);
                 }
                 else {
-                    System.out.printf("Investment rate: %f\n", c);
+                    System.out.printf("Investment rate: %,1f\n", c);
+                    System.out.println();
                     i = 0;
                 }
             }
             else {
-                System.out.printf("Error: Investment rate should be a valid number. You entered: %f\n", c);
+                String d = kbd.next();
+                System.out.println("Error: Investment rate should be a valid number. You entered: " + d);
             }
         }
-        return i;
     }
 
-    public static double validateInvestmentTime(Scanner kbd){
-        double i = 3;
+    public static void getValidNumberOfYears(Scanner kbd){
+        int i = 3;
         while (i == 3) {
+            
             System.out.print("Enter the Investment time: ");
-            double c = kbd.nextDouble();
             if (kbd.hasNextDouble()) {
+                double c = kbd.nextDouble();
                 if (c < 0) {
-                    System.out.printf("Error: Investment time should be greater than zero. You entered: %f\n", c);
+                    System.out.printf("Error: Investment time should be greater than zero. You entered: %.1f\n", c);
                 }
                 else {
-                    System.out.printf("Investment time: %f\n", c);
+                    System.out.printf("Investment time: %.1f\n", c);
+                    System.out.println();
                     i = 0;
                 }
             }
             else {
-                System.out.printf("Error: Investment time should be a valid number. You entered: %f\n", c);
+                String d = kbd.next();
+                System.out.println("Error: Investment time should be a valid number. You entered: " + d);
+
             }
         }
-        return i;
     }
-    public static int validateInvestmentYears(Scanner kbd){
+    public static void getValidCompund(Scanner kbd){
         int i = 4;
         while (i == 4) {
             System.out.print("Enter the have many year to compound investment: ");
-            int c = kbd.nextInt();
+
             if (kbd.hasNextInt()) {
+                int c = kbd.nextInt();
                 if (c < 0) {
-                    System.out.printf("Error: Compounded years should be greater than zero. You entered: %f\n", c);
-                }
-                if (c < 0) {
-                    System.out.printf("Error: Compounded years should be greater than zero. You entered: %f\n", c);
+                    System.out.println("Error: Compounded years should be greater than zero. You entered: " + c);
                 }
                 else {
-                    System.out.printf("Investment rate: %f\n", c);
+                    System.out.println("Investment rate: " + c);
+                    System.out.println();
                     i = 0;
                 }
             }
             else {
-                System.out.printf("Error: Compounded years should be a valid number. You entered: %f\n", c);
+                String d = kbd.next();
+                System.out.println("Error: Compounded years should be a valid number. You entered: " + d);
             }
         }
-        return i;
     }
 
 
