09e1b0a519af77d36a0759a9011e352c0f1419fa
Zakiy Manigo <zakiymanigo@gmail.com>
2023-04-25 11:35:21 -0400

    text areas, label, and JFrame

---
+++ src/GUI/Main.java
@@ -0,0 +1,66 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.net.http.WebSocket;
+
+public class Main implements ActionListener {
+    JTextArea input, output;
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.go();
+    }
+
+    private void go(){
+        JFrame frame = new JFrame("CS219 Text Processor");
+        frame.setSize(1000,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        JLabel header = new JLabel("CS219 Text Processor");
+        header.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        header.setBackground(Color.BLUE);
+        header.setOpaque(true);
+        frame.add(header);
+
+        //combine labe//text area into a JPanel
+        JPanel input_panel = new JPanel();
+        //input label
+        JLabel input_label = new JLabel("input");
+        input_panel.add(input_label);
+        //make input text area
+        input = new JTextArea(10,60);
+        input.setFont(new Font("Arial",Font.PLAIN,20));
+        input_panel.add(input);
+        frame.add(input_panel);
+
+        JPanel output_panel = new JPanel();
+        //output label
+        JLabel output_label = new JLabel("output");
+        output_panel.add(output_label);
+        //make output text area
+        output = new JTextArea(10,60);
+        output.setFont(new Font("Arial",Font.PLAIN,20));
+        output_panel.add(output);
+        frame.add(output_panel);
+
+        //Add a button to the bottom
+        JButton submit = new JButton("submit");
+        submit.setBackground(Color.YELLOW);
+        submit.addActionListener(this);
+        frame.add(submit);
+
+
+        frame.setVisible(true);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String s = input.getText().toUpperCase();
+        output.setText(s);
+    }
+}
