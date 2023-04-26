254195845cbd9f29995bf902306e5db1e3d7d3b6
zakiymanigo <zakiymanigo@gmail.com>
2023-04-26 00:36:56 -0400

    HW8

--- src/HW8/WeatherClient.java
+++ src/HW8/WeatherClient.java
@@ -5,15 +5,15 @@ import ZipCodes.ZipCode;
 
 import java.util.Scanner;
 
-public class WeatherClient extends WeatherObervation implements WeatherClientInterface{
+public class WeatherClient implements WeatherClientInterface {
 
-
-    public WeatherClient(double temperature, double windspeed, double humidity, String cloudcover) {
-        super(temperature, windspeed, humidity, cloudcover);
-    }
 
     @Override
     public WeatherObervation getWeather(ZipCode code) {
+        String cloudcover = "";
+        double humidity = 0;
+        double windspeed = 0;
+        double temperature = 0;
 
         String lat = String.valueOf(code.getCoord().getLat());
         String lng = String.valueOf(code.getCoord().getLng());
@@ -22,26 +22,25 @@ public class WeatherClient extends WeatherObervation implements WeatherClientInt
         if (s == null) {
             return null;
         }
-        else {
-            System.out.println(s);
-            if(s.hasNext("tempature")){
-                this.temperature = s.nextDouble();
+        while (s.hasNextLine()) {
+            if (s.hasNext("clouds")) {
+                String cl = s.toString();
+                cloudcover = cl.substring(10, cl.length() - 2);
+            } else {
+                cloudcover = "n/a";
             }
-            if(s.hasNext("windSpeed")){
-                this.windspeed = s.nextDouble();
+            if (s.hasNext("tempature")) {
+                temperature = s.nextDouble();
             }
-            if(s.hasNext("humidity")){
-                this.humidity = s.nextDouble();
+            if (s.hasNext("humidity")) {
+                humidity = s.nextDouble();
             }
-            if(s.hasNext("clouds")){
-                cloudcover = s.toString();
-                this.cloudcover = cloudcover.substring(10,cloudcover.length()-2);
+            if (s.hasNext("windSpeed")) {
+                windspeed = s.nextDouble();
             }
-            else {
-                this.cloudcover = "n/a";
-            }
-            return new WeatherObervation(temperature, windspeed, humidity, cloudcover);
+
         }
+        return new WeatherObervation(temperature, windspeed, humidity, cloudcover);
     }
 }
 
