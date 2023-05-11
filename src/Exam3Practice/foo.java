f39edecbba7dd141bdc3849c23a94f040d9277d6
zakiymanigo <zakiymanigo@gmail.com>
2023-05-10 20:51:22 -0400

    exam 3 prac

---
+++ src/Exam3Practice/foo.java
@@ -0,0 +1,9 @@
+package Exam3Practice;
+
+public class foo {
+    protected int x;
+    private int y;
+    public static int z = -99;
+
+
+}
---
+++ src/Exam3Practice/practice.java
@@ -0,0 +1,37 @@
+package Exam3Practice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println(24 / 5);
+        System.out.println(3/5);
+
+//        ArrayList<String> courses = new ArrayList<>();
+//        Scanner kbd = new Scanner(System.in);
+//        boolean target = true;
+//        while (target){
+//            String input = kbd.nextLine();
+//            if (input.equals("done")){
+//                target = false;
+//            }
+//            else {
+//                courses.add(input);
+//            }
+//        }
+//        System.out.println(courses);
+
+        System.out.println(7%26);
+        System.out.println(Math.floorDiv(-2,3));
+        System.out.println(Math.floorMod(7,26));
+        System.out.println('b'-'d');
+        System.out.println(4-9+"94");
+        System.out.println("23"+2+-3);
+        System.out.println(4321%10);
+
+        String s = "hello";
+        s. = "d";
+
+    }
+}
